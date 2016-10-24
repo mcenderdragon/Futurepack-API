d322bb3482d13c3e4fc2aad8b142aa7051a9fccb
mcenderdragon <goto.minecraft@googlemail.com>
2016-10-24 20:07:19 +0200

    added Event

---
+++ futurepack/api/event/ResearchPageRegisterEvent.java
@@ -0,0 +1,11 @@
+package futurepack.api.event;
+
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+/**
+ * Listen to this Event to add your own {@link futurepack.common.research.ResearchPage} to the Research Overlay
+ */
+public class ResearchPageRegisterEvent extends Event
+{
+
+}
