2bc5f67184c5b4d89c013051a8b48611267aca22
mcenderdragon <goto.minecraft@googlemail.com>
2018-10-12 12:55:03 +0200

    imrpoved ItemPredicates

--- futurepack/api/ItemPredicates.java
+++ futurepack/api/ItemPredicates.java
@@ -1,5 +1,8 @@
 package futurepack.api;
 
+import java.util.List;
+
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 import com.google.common.base.Predicate;
@@ -16,4 +19,17 @@ public abstract class ItemPredicates implements Predicate<ItemStack>
 	 */
 	@Nullable
 	public abstract ItemStack getRepresentItem();
+	
+	public boolean apply(ItemStack item, boolean ignoreStackSize)
+	{
+		if(ignoreStackSize)
+			return apply(item);
+		else
+			return apply(item) & item.getCount() >= getMinimalItemCount(item);
+	}
+	
+	public abstract int getMinimalItemCount(ItemStack item);
+	
+	public abstract List<ItemStack> collectAcceptedItems(@Nonnull List<ItemStack> list);
+	
 }
