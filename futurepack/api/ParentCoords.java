030ff27cfd025fa3f73d26b133c97b3d794cd9dc
mcenderdragon <goto.minecraft@googlemail.com>
2018-10-12 12:55:22 +0200

    parent cordes depth is now stored

--- futurepack/api/ParentCoords.java
+++ futurepack/api/ParentCoords.java
@@ -8,15 +8,22 @@ import net.minecraft.util.math.BlockPos;
 public class ParentCoords extends BlockPos
 {
 	private final ParentCoords parent;
+	private final int depth;
 	
 	public ParentCoords(BlockPos coords, ParentCoords parent)
     {
         super(coords);
         this.parent = parent;
+        this.depth = parent == null ? 0 : parent.getDepth()+1;
     }
 	
 	public ParentCoords getParent()
 	{
 		return parent;
+	}
+	
+	public int getDepth()
+	{
+		return depth;
 	}
 }
